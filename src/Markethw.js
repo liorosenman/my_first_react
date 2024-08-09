bad617eacfbcb95d8544214700d61e68ce16fd15
liorosenman <lros209@gmail.com>
2024-08-09 15:09:52 +0300

    commit-8

--- src/Markethw.js
+++ src/Markethw.js
@@ -7,43 +7,57 @@ const Markethw = () => {
     const [cart, setCart] = useState([])
 
     useEffect(() => {
-        console.log("Cart changed")
+        console.table(cart)
     }, [cart])
     
     
     const addToCart = (prod, delta) => {
+        let index = isProductInCart(prod.id)
         if (delta == 1){          
-            if (isProductInCart(prod.id)){
-                let tempProduct = cart[]
+            if (index != -1){
+                let tempProduct = cart[index];
                 tempProduct.amount += delta
                 setCart([...cart])
-            }else
+            }else{
+                prod.amount = 1
                 setCart([...cart, prod])
+            }
         }
         else{
-            let tempProduct = cart[prod.id]
+            console.log("This is reduction");          
+            let tempProduct = cart[index]
             tempProduct.amount += delta
-            if(tempProduct.amount == 0){
-                let updatedCart = cart.filter(item => item.id != prod.id)
-            setCart([...cart, updatedCart])}       
+            if(tempProduct.amount == 0)
+                setCart([...cart.filter(item => item.id != prod.id)])
+            else
+                setCart([...cart])     
     }
 }
 
     const isProductInCart = (idToCheck) => {
         for (let i = 0; i < cart.length; i++)
             if (idToCheck == cart[i].id)
-                return true;
-        return false;
+                return i;
+        return -1;
     }
     
   return (
     <div>
             {products.map(prod =>
-             <div key={prod.id}><h3>Title: {prod.desc}, Price: {prod.price}</h3><button onClick={addToCart(prod, 1)}>ADD</button></div>)}
+             <div key={prod.id}><h3>Title: {prod.desc}, Price: {prod.price}</h3><button onClick={() => addToCart(prod, 1)}>ADD</button></div>)}
              <hr></hr>
              <h3>CART:</h3>
+             <div>
+            {cart.map(prod =>
+            <div>
+                <button onClick={() => addToCart(prod, -1)}>-</button>
+                {prod.desc}, {prod.price}, {prod.amount}
+                <button onClick={() => addToCart(prod, 1)}>+</button>
+             </div>
+            )}</div>
     </div>
-  )
+  
+)
 }
 
 export default Markethw
