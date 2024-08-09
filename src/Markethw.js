6951951ac37220130de2d17de06a950e55a155eb
liorosenman <lros209@gmail.com>
2024-08-09 12:25:28 +0300

    commit-7

--- src/Markethw.js
+++ src/Markethw.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Markethw = () => {
     const products = [{id:0, desc:"apple", price:3},
                 {id:1, desc:"rice", price:10},
                 {id:2, desc:"milk", price:7}];
     const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        console.log("Cart changed")
+    }, [cart])
+    
     
     const addToCart = (prod, delta) => {
-        console.log("you try to do");
-        if (delta == 1){
-            console.log("you are adding");          
-            if (isProductInCart(prod.id))
-                setCart(cart[prod.id].amount += delta)
-            else
-                setCart(...cart, prod)
+        if (delta == 1){          
+            if (isProductInCart(prod.id)){
+                let tempProduct = cart[]
+                tempProduct.amount += delta
+                setCart([...cart])
+            }else
+                setCart([...cart, prod])
         }
-        console.log(cart);
-        
+        else{
+            let tempProduct = cart[prod.id]
+            tempProduct.amount += delta
+            if(tempProduct.amount == 0){
+                let updatedCart = cart.filter(item => item.id != prod.id)
+            setCart([...cart, updatedCart])}       
     }
+}
 
     const isProductInCart = (idToCheck) => {
         for (let i = 0; i < cart.length; i++)
@@ -29,7 +39,7 @@ const Markethw = () => {
   return (
     <div>
             {products.map(prod =>
-             <div><h3>Title: {prod.desc}, Price: {prod.price}</h3><button onClick={addToCart(prod, 1)}>ADD</button></div>)}
+             <div key={prod.id}><h3>Title: {prod.desc}, Price: {prod.price}</h3><button onClick={addToCart(prod, 1)}>ADD</button></div>)}
              <hr></hr>
              <h3>CART:</h3>
     </div>
